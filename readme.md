e2f4c9f0fa2e21d2e9c479c5933ce3fe517c48fb
Vasu Jain <vasu.jain@live.in>
2015-03-12 00:21:19 -0700

    URL Redirect Added

--- readme.md
+++ readme.md
@@ -1,1 +1,52 @@
+Objective
+*********
+Url like 
+    https://www.youtube.com/watch?v=5lRcfg_XUfQ
+to ==>
+    https://www.youtube.com/embed/5lRcfg_XUfQ
+    
+Details
+*******
+"name" : "uFull",
+"description" : "Chrome Extension to turn youtube videos to full screen within browser",
+"version" : "0.1.0",
+"permissions" : [
+    "tabs",
+    "bookmarks",
+    "activeTab"
+]
+    
+Icon License
+************
 Icons under CC Attribution-Noncommercial-No Derivate 4.0 license from http://www.iconarchive.com/show/circle-icons-by-martz90/video-camera-icon.html
+
+Regex Match Object
+******************
+    [0]https://www.youtube.com/watch?v=BKOlYDyo3sA
+    [1]https://
+    [2]www.youtube.com
+    [3]www.
+    [4]/watch?v=
+    [5]BKOlYDyo3sA
+    
+Tab Object
+***********
+Array[1]
+    0: Object
+        active: true
+        favIconUrl: "https://s.ytimg.com/yts/img/favicon_32-vflWoMFGx.pn"
+        height: 753
+        highlighted: true
+        id: 185
+        incognito: false
+        index: 9
+        pinned: false
+        selected: true
+        status: "complete"
+        title: "How it works - YouTube"
+        url: "https://www.youtube.com/watch?v=WB370ewOklw"
+        width: 1439
+        windowId: 22
+        __proto__: Object
+        length: 1
+    __proto__: Array[0]
--- resource.js
+++ resource.js
@@ -1,67 +1,60 @@
-/* Created by vasujain on 03/08/15. */
+/** Author Details
+ * @name Vasu Jain
+ * @email vj@brk.im
+ * @date 03/08/15
+ * @license Open Source
+ */
 
-// Run script as soon as the document's DOM is ready.
+/**
+ * Global Variables
+ * @type {string} redirectUrl : Setting up a default Redirect URL
+ * @type {string} divOutputHtml : Sets Output Div HTML content
+ *
+ */
+var redirectUrl = "http://www.google.com";
+var divOutputHtml = "";
 
-var redirectUtubeUrl = "";
-
-document.addEventListener('DOMContentLoaded', function() {
+/**
+ * Run script as soon as the document's DOM is ready.
+ */
+document.addEventListener('DOMContentLoaded', function () {
     getCurrentTab();
-});
-document.getElementById('convertUrl').addEventListener('click', function(){
-    redirectUrl();
+    //document.getElementById('buttonId').addEventListener('click', function(){}
 });
 
-
-chrome.extension.onRequest.addListener(function(request, sender) {
-    chrome.tabs.update(sender.tab.id, {url: request.redirect});
-});
-
-
+/**
+ * Get the current tab from where the Extn. is invoked
+ */
 function getCurrentTab() {
-    return chrome.tabs.query({currentWindow : true, active:true}, function (tabs){
-        checkCurrentTab(tabs[0].url);
+    return chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        checkCurrentTab(tabs[0].url, tabs[0].id);
     });
 }
 
-function redirectUrl() {
-    chrome.tabs.update(tab.id, {url: "https://www.youtube.com/"});
-    //chrome.extension.sendRequest({redirect : redirectUtubeUrl});
-}
+/**
+ * RegEx stuff and redirection to embed URL for youtube
+ */
+function checkCurrentTab(tabUrl, tabId) {
 
-function checkCurrentTab(tabUrl) {
+    //Nasty Youtube URL Regex... can be improved for mobile URLs and international Domains like .in,.uk etc
     var regEx = /(http\:\/\/|https\:\/\/)?((www\.)youtube\.com|youtu\.?be)(\/watch\?v=)(\w{11})/;
     var match = regEx.exec(tabUrl);
 
-    var appendOutPut = "";
-    if(match == null) {
-        appendOutPut = "<div style='color: #D8000C;background-color: #FFBABA;'>Invalid URL: " + tabUrl + "</div>";
+    //Check if URL is a valid YouTube URL
+    if (match == null) {
+        divOutputHtml = "<div style='color: #D8000C;background-color: #FFBABA;'>Invalid URL: " + tabUrl + "</div>";
+        document.getElementById("convertUrl").innerHTML = divOutputHtml;
     } else {
-        if(match.length >= 6) {
+        //Sanity check... to set up the new URL
+        if (match.length >= 6) {
             match[4] = "\/embed\/";
         }
-        appendOutPut += "<div style='color: #4F8A10;background-color: #DFF2BF;'>" + match[0] + "</div>";
-        redirectUtubeUrl = match[1] + match[2] + match[4] + match[5];
-        appendOutPut += "<div style='color: #4F8A10;background-color: #DFF2BF;'>" + redirectUrl + "</div>";
+        divOutputHtml += "<div style='color: #4F8A10;background-color: #DFF2BF;'>" + match[0] + "</div>";
+        redirectUrl = match[1] + match[2] + match[4] + match[5];
+        divOutputHtml += "<div style='color: #4F8A10;background-color: #DFF2BF;'>" + redirectUrl + "</div>";
+        document.getElementById("convertUrl").innerHTML = divOutputHtml;
+        chrome.tabs.update(tabId, {url: redirectUrl});
     }
-    document.getElementById("output").innerHTML = appendOutPut;
-
-
-
-
-        /**
-     * 6
-         [0]https://www.youtube.com/watch?v=BKOlYDyo3sA
-         [1]https://
-         [2]www.youtube.com
-         [3]www.
-         [4]/watch?v=
-         [5]BKOlYDyo3sA
-
-
-     https://www.youtube.com/watch?v=BKOlYDyo3sA,https://,www.youtube.com,www.,/embed/,BKOlYDyo3sA
-     */
-
-    //https://www.youtube.com/watch?v=5lRcfg_XUfQ
-    //https://www.youtube.com/embed/5lRcfg_XUfQ
 
 }
+
